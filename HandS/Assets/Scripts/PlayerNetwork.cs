4f542d8ae29f17981b72c27a901ff0152c38dd90
Dino Ehman <dino.ehman@fer.hr>
2019-12-11 13:17:47 +0100

    multiplayer

---
+++ HandS/Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    public GameObject firstPersonCharacter;
+    public GameObject[] characterModel;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<FirstPersonController>().enabled = true;
+        firstPersonCharacter.SetActive(true);
+
+        foreach(GameObject go in characterModel)
+        {
+            go.SetActive(false);
+        }
+    }
+}
